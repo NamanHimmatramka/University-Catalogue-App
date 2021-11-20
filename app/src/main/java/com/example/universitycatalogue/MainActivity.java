54ec47fddefa67e5a781e603b76a4173fd5ac38b
Naman Himmatramka <himmatramka2003@gmail.com>
2021-11-20 13:34:46 +0530

    Finishing changes

--- app/src/main/java/com/example/universitycatalogue/MainActivity.java
+++ app/src/main/java/com/example/universitycatalogue/MainActivity.java
@@ -4,28 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
+    Button searchButtonDefault;
     EditText countryName;
     Button searchButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        countryName = findViewById(R.id.country_name);
-        searchButton = findViewById(R.id.searchButton);
+        searchButtonDefault = findViewById(R.id.searchButtonDefault);
+        countryName=findViewById(R.id.countryName);
+        searchButton=findViewById(R.id.searchButton);
 
-        searchButton.setOnClickListener(new View.OnClickListener() {
+        Locale localCountry = Locale.getDefault();
+        String countryCodeValue=localCountry.getDisplayCountry();
+
+        searchButtonDefault.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, CollegeList.class);
-                intent.putExtra("countryName", countryName.getText().toString());
+                intent.putExtra("countryName", countryCodeValue);
                 startActivity(intent);
+            }
+        });
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CollegeList.class);
+                String toPass = null;
+                if(!countryName.getText().toString().isEmpty()) {
+                    toPass = countryName.getText().toString().substring(0, 1).toUpperCase() + countryName.getText().toString().substring(1);
+                    intent.putExtra("countryName", toPass);
+                    startActivity(intent);
+                }
             }
         });
     }
